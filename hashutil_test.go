e2ca40a594eb1d56638a3cb11894f381d59a1d7a
Jan Seidl <seidl@avast.com>
2017-09-21 16:45:52 +0200

    remove 1.2-1.4 from travis, because don't implement New512_256 function

--- hashutil_test.go
+++ hashutil_test.go
@@ -66,13 +66,6 @@ func TestMixOtherKindOfHash(t *testing.T) {
 }
 
 func TestMixOtherKindOfHashButWithSameLength(t *testing.T) {
-	goVersion := runtime.Version()
-	t.Log(goVersion)
-	// e.g. go1.2.2 linux/amd64
-	if goVersion[2] == 1 && goVersion[4] < 5 {
-		t.Skip("Go 1.4 and less not implement New512_256 function")
-	}
-
 	hash1 := EmptyHash(sha512.New512_256())
 	hash2 := EmptyHash(sha256.New())
 
