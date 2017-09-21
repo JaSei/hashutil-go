949bd4a5623f204aa886bff6f69cc56d9a1ae1bf
Jan Seidl <seidl@avast.com>
2017-09-21 15:19:44 +0200

    more generic hashutil (1.0.0)

--- hashutil.go
+++ hashutil.go
@@ -1,49 +1,101 @@
+/*
+Hashutil is collection of common hash (crypto) types.
+It also contains some helper functions for hash manipulation.
+
+
+
+SYNOPSIS
+
+	import (
+		"github.com/avast/hashutil-go",
+		"crypto/md5"
+	)
+
+	hash, _ := hashutil.StringToHash(md5.New(), "d41d8cd98f00b204e9800998ecf8427e")
+	otherHash, _ := hashutil.BytesToHash(md5.New(), md5.New().Sum(nil))
+	// or
+	// otherhash := hashutil.EmptyHash(md5.New())
+
+	if hash.Equal(otherHash) {
+		// do something
+	}
+*/
 package hashutil
 
 import (
 	"bytes"
-	"crypto"
 	"encoding/hex"
 	"fmt"
+	"hash"
+	"reflect"
 )
 
-type Hash []byte
+// hashutil.Hash represents hash in native form and kind of hash
+type Hash struct {
+	hash []byte
+	kind hash.Hash
+}
 
-func StringToHash(hashType crypto.Hash, str string) (Hash, error) {
-	bytes, err := hex.DecodeString(str)
+// StringToHash validate and convert hexString of hash function implement hash.Hash to hashutil.Hash representation
+func StringToHash(hash hash.Hash, hexString string) (Hash, error) {
+	bytes, err := hex.DecodeString(hexString)
 	if err != nil {
-		return nil, err
+		return Hash{}, err
 	}
 
-	if err := checkLen(hashType, bytes); err != nil {
-		return nil, err
+	if err := checkLen(hash, bytes); err != nil {
+		return Hash{}, err
 	}
 
-	return bytes, nil
+	return Hash{bytes, hash}, nil
 
 }
 
-func checkLen(hashType crypto.Hash, bytes []byte) error {
+// BytesToHash validate and convert []byte of hash function implement hash.Hash interface to hashutil.Hash representation
+func BytesToHash(hash hash.Hash, bytes []byte) (Hash, error) {
+	if err := checkLen(hash, bytes); err != nil {
+		return Hash{}, err
+	}
+
+	return Hash{bytes, hash}, nil
+}
+
+func checkLen(hash hash.Hash, bytes []byte) error {
 	actualLength := len(bytes)
-	if actualLength != hashType.Size() {
-		return fmt.Errorf("Hash function #%d must have a length of %d bytes (actual have %d)", hashType, hashType.Size(), actualLength)
+	if actualLength != hash.Size() {
+		return fmt.Errorf(
+			"Hash function represent by '%s' must have a length of %d bytes (actual have %d)",
+			reflect.TypeOf(hash),
+			hash.Size(),
+			actualLength,
+		)
 	}
 
 	return nil
 }
 
-func BytesToHash(hashType crypto.Hash, bytes []byte) (Hash, error) {
-	if err := checkLen(hashType, bytes); err != nil {
-		return nil, err
-	}
-
-	return bytes, nil
+// EmptyHash return hashutil.Hash of empty (zero-length) hash function implement hash.Hash
+func EmptyHash(hash hash.Hash) Hash {
+	return Hash{hash.Sum(nil), hash}
 }
 
+// Equal two hashutil.Hash, return true if hashes are same
 func (hash Hash) Equal(otherHash Hash) bool {
-	return bytes.Compare(hash, otherHash) == 0
+	return reflect.TypeOf(hash.kind) == reflect.TypeOf(otherHash.kind) &&
+		bytes.Equal(hash.hash, otherHash.hash)
 }
 
+// String return hexString representation in lower case of hashutil.Hash
 func (hash Hash) String() string {
-	return hex.EncodeToString(hash)
+	return hex.EncodeToString(hash.hash)
+}
+
+// ToBytes return []byte of hashutil.Hash
+func (hash Hash) ToBytes() []byte {
+	return hash.hash
+}
+
+// IsEmpty return true, if hashutil.Hash represents empty (zero-length) hash (of given hash.Hash)
+func (hash Hash) IsEmpty() bool {
+	return hash.Equal(EmptyHash(hash.kind))
 }
--- hashutil_test.go
+++ hashutil_test.go
@@ -1,24 +1,27 @@
 package hashutil
 
 import (
-	"crypto"
+	"crypto/md5"
+	"crypto/sha256"
+	"crypto/sha512"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-//const testEmptyMd5String = "d41d8cd98f00b204e9800998ecf8427e"
+const testEmptyMd5String = "d41d8cd98f00b204e9800998ecf8427e"
 
 func TestStringToHashAndEqual(t *testing.T) {
-	_, err := StringToHash(crypto.MD5, "")
+	_, err := StringToHash(md5.New(), "")
 	assert.Error(t, err, "Empty string isn't valid hash")
+	assert.Equal(t, err.Error(), "Hash function represent by '*md5.digest' must have a length of 16 bytes (actual have 0)")
 
-	_, err = StringToHash(crypto.MD5, "x41d8cd98f00b204e9800998ecf8427e")
+	_, err = StringToHash(md5.New(), "x41d8cd98f00b204e9800998ecf8427e")
 	assert.Error(t, err, "x isn't isn't valid char in hash")
 
-	hash, err := StringToHash(crypto.MD5, testEmptyMd5String)
+	hash, err := StringToHash(md5.New(), testEmptyMd5String)
 	assert.NoError(t, err, "Convert string to Md5 without errors")
 
-	hash2, err := StringToHash(crypto.MD5, "D41D8CD98F00B204E9800998ECF8427E")
+	hash2, err := StringToHash(md5.New(), "D41D8CD98F00B204E9800998ECF8427E")
 	assert.NoError(t, err, "Convert string to Md5 without errors")
 
 	assert.Equal(t, hash, hash2, "Upper and lower of same Md5 are equal")
@@ -27,9 +30,48 @@ func TestStringToHashAndEqual(t *testing.T) {
 }
 
 func TestBytesToHash(t *testing.T) {
-	_, err := BytesToHash(crypto.MD5, []byte{})
+	_, err := BytesToHash(md5.New(), []byte{})
 	assert.Error(t, err)
 
-	_, err = BytesToHash(crypto.MD5, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+	_, err = BytesToHash(md5.New(), []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
 	assert.NoError(t, err)
+}
+
+func TestEmptyHash(t *testing.T) {
+	hash, err := StringToHash(md5.New(), testEmptyMd5String)
+	assert.NoError(t, err)
+	assert.Equal(t, hash, EmptyHash(md5.New()))
+}
+
+func TestHashToString(t *testing.T) {
+	hash, err := StringToHash(md5.New(), testEmptyMd5String)
+	assert.NoError(t, err)
+	assert.Equal(t, hash.String(), testEmptyMd5String)
+}
+
+func TestIsEmpty(t *testing.T) {
+	hash, err := StringToHash(md5.New(), testEmptyMd5String)
+	assert.NoError(t, err)
+	assert.True(t, hash.IsEmpty())
+}
+
+func TestMixOtherKindOfHash(t *testing.T) {
+	hash1, err := StringToHash(md5.New(), testEmptyMd5String)
+	assert.NoError(t, err)
+
+	hash2 := EmptyHash(sha256.New())
+
+	assert.False(t, hash1.Equal(hash2))
+}
+
+func TestMixOtherKindOfHashButWithSameLength(t *testing.T) {
+	hash1 := EmptyHash(sha512.New512_256())
+	hash2 := EmptyHash(sha256.New())
+
+	assert.False(t, hash1.Equal(hash2), "Sha512_256 is other hash then Sha256")
+
+	hashFake, err := BytesToHash(sha256.New(), hash1.ToBytes())
+
+	assert.NoError(t, err)
+	assert.False(t, hashFake.Equal(hash1), "Hash with same content, but other hash function are not equal")
 }
